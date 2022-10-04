149f3c984135caf6000891b45c979fbbd32fd60f
wjdskagjs1 <wjdskagjs1@naver.com>
2022-10-05 02:18:30 +0900

    policy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const discordClient = new Discord.Client();
+
 const { TOKEN } = require('./config.json');
 const fs = require('fs');
 const article = fs.readFileSync("README.md").toString();
@@ -25,18 +26,18 @@ discordClient.on('message', msg => {
 	const currentChannel = channel.name;
 	const prefix = '<>';
 
-	/*
+	
 	//테스트용
-	if(author.username === 'fact'){
-		console.log(content);
-	}
-	*/
+	// if(author.username === 'fact'){
+	// 	console.log(content);
+	// }
+	
 
 	if(content.substring(0,2) === prefix){
 		const command = content.substring(2);
 		
 		if(command === 'hellothisisverification'){
-			channel.send('fact#4858(353467095876501504)');
+			channel.send();
 			return;
 		}
 
@@ -54,6 +55,18 @@ discordClient.on('message', msg => {
         channel.send("\`\`\`"+article+"\`\`\`");
         return;
     }
+	const unified_emoji_ranges = ['\ud83c[\udf00-\udfff]','\ud83d[\udc00-\ude4f]','\ud83d[\ude80-\udeff]'];
+
+	const reg = new RegExp(`^(${unified_emoji_ranges.join('|')})`, 'g');
+
+	if(content.match(reg) || content.startsWith('<:')){ // 이모티콘
+		return;
+	}
+
+	if(content.length === 1){ // 1글자
+		return;
+	}
+
     if(content.endsWith('?')){
 		let tempReplies = replies;
 		if(['되', '된', '될', '됨', '됩', '됫', '됬', '돼', '됀', '됄', '됌', '됍', '됐', '됐'].some((word)=>content.includes(word))){
