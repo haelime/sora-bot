ef9bafce66566b2c72ea1c3cc3de0a5d1bb38054
wjdskagjs1 <wjdskagjs1@naver.com>
2023-01-02 12:30:08 +0900

    sora-bot refactoring

--- index.js
+++ index.js
@@ -1,83 +1,101 @@
-const Discord = require('discord.js');
-const discordClient = new Discord.Client();
+const {TOKEN, CLIENT_ID} = require('./config.json');
 
-const { TOKEN } = require('./config.json');
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+{
+	name: '도움말',
+	description: '사용법 설명',
+}
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+try {
+	console.log('Started refreshing application (/) commands.');
+
+	await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+	console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+	console.error(error);
+}
+})();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+] });
+
 const fs = require('fs');
 const article = fs.readFileSync("README.md").toString();
-
-const replies = require('./replies.json');
-
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+const replies = [
+	"그래.",
+	"맞아.",
+	"어.",
+	"아니."
+];
+const TEST_MODE = false;
+const isTest = function(author){
+	if(!TEST_MODE) return false;
+	
+	return author.username !== "fact";
 }
+
+const getRandomInt = function (min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
+
 const randomMessage = function(msgList){
-    return msgList[getRandomInt(0, msgList.length)];
+	return msgList[getRandomInt(0, msgList.length)];
 }
 
-discordClient.on('ready', () => {
-  console.log(`Logged in as ${discordClient.user.tag}!`);
-});
-
-discordClient.on('message', msg => {
-	const {content, channel, author} = msg;
-	const currentChannel = channel.name;
-	const prefix = '<>';
-
-	
-	//테스트용
-	// if(author.username === 'fact'){
-	// 	console.log(content);
-	// }
-	
-
-	if(content.substring(0,2) === prefix){
-		const command = content.substring(2);
+client.on('ready', () => {
+	try {
+		console.log(`Logged in as ${client.user.tag}!`);
+	} catch (error) {
 		
-		if(command === 'hellothisisverification'){
-			channel.send();
-			return;
-		}
-
-		if(command === '도움말'){
-			channel.send("\`\`\`"+article+"\`\`\`");
-			return;
-		}
-		return;
 	}
-
-    if(!currentChannel.includes('소라고동') && !currentChannel.includes('소라고둥') || author.bot ){
-        return;
-    }
-    if(content.includes('도움말')){
-        channel.send("\`\`\`"+article+"\`\`\`");
-        return;
-    }
-	const unified_emoji_ranges = ['\ud83c[\udf00-\udfff]','\ud83d[\udc00-\ude4f]','\ud83d[\ude80-\udeff]'];
-
-	const reg = new RegExp(`^(${unified_emoji_ranges.join('|')})`, 'g');
-
-	if(content.match(reg) || content.startsWith('<:')){ // 이모티콘
-		return;
-	}
-
-	if(content.length === 1){ // 1글자
-		return;
-	}
-
-    if(content.endsWith('?')){
-		let tempReplies = replies;
-		if(['되', '된', '될', '됨', '됩', '됫', '됬', '돼', '됀', '됄', '됌', '됍', '됐', '됐'].some((word)=>content.includes(word))){
-			tempReplies = [...tempReplies, '안돼.', '안.돼.'];
-		} 
-        channel.send(randomMessage(tempReplies));
-    }else{
-        channel.send('다시 한 번 물어봐.');
-    }
 });
 
-discordClient.on("error", () => { console.log("error"); });
+client.on('messageCreate', message => {
+	try {
+		const {content, channel, author} = message;
+		
+		if(['소라고둥', '소라고동'].every(word=>!channel.name.includes(word))) return;
+		if(author.bot) return;
 
-discordClient.login(TOKEN);
+		//테스트용
+		if(isTest(author)) return;
+
+		if(content.endsWith('?')){
+			message.reply(randomMessage(replies));
+		}else{
+			message.reply('다시 한 번 물어봐.');
+		}
+
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+client.on('interactionCreate', async interaction => {
+	try {
+		if(isTest(interaction.user)) return;
+
+		if (interaction.isChatInputCommand()){
+			if (interaction.commandName === '도움말' ) {
+				await interaction.reply({ content: "\`\`\`"+article+"\`\`\`", ephemeral: true });
+			}
+		}
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+client.login(TOKEN);
